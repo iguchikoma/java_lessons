a89c4ad77ee1c8670e3b4942b945765e596f0bfa
Takashi Iguchi <iguchi.t@gmail.com>
2015-05-05 11:50:29 +0900

    Add a description for the HelloWorld class
    Add a description for the HelloWorld class.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,8 @@
+/**
+ * HelloWorld is a class to show hello world
+ * @author takashi iguchi
+ *
+ */
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello World!");
